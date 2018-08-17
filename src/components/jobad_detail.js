91fa40f4dafcac4cb96bb63368234d76d69f83a4
rapahaeru <rapahaeru@gmail.com>
2018-08-17 16:13:55 -0300

    estrutura a base responsiva de css do jobdetails

--- src/components/jobad_detail.js
+++ src/components/jobad_detail.js
@@ -1,13 +1,9 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component} from 'react';
 
-import Phrase from '../components/phrase'
+import { Logo } from '@cathodevel/quantum';
 
 import { connect } from 'react-redux';
-// import { bindActionCreators } from 'redux';
-
 import { clickButton } from '../actions/index';
-
-import ButtonNative from '../containers/button_native'
 
 class JobadDetail extends Component {
 
@@ -19,14 +15,46 @@ class JobadDetail extends Component {
     console.log(this.props);
 
     return (
-      <Fragment>
-        <div className="jobad-container">
-          <Phrase content={this.props.contentClicked} />
-          <div>
-            <ButtonNative actionClick={ this.buttonClicked } />
-          </div>
-        </div>
-      </Fragment> 
+	<div className="jobad-container">
+		<header className="jobad-detail-header">
+			<Logo.Candidate className="logo" />
+			<div className="user-area">
+				<img src="https://www.catho.com.br/images/site/avatarM.jpg" className="user-thumb" alt="descrição da imagem" />
+				<strong className="user-info">
+					nome do candidato
+					<a href="#">sair</a>
+				</strong>
+			</div>
+		</header>
+		<section className="jobad-description">
+			<h1>Título da vaga</h1>
+			<strong className="jobad-info-vacant">
+				<span className="total">1 vaga</span>
+				<time pubdate="2018-07-23"> - segunda, 23/07</time>
+			</strong>
+
+			<em className="jobad-info">
+				<span className="salary">A combinar </span>
+				<address>São Paulo <abbr title="São Paulo">SP</abbr></address>
+			</em>
+
+			<strong className="company-name">Empresa confidencial</strong>
+
+			<section className="description">
+				<section className="curriculum-status-desktop">
+					<button>vaga expirada</button>
+				</section>
+				<p>Mussum Ipsum, cacilds vidis litro abertis. Manduma pindureta quium dia nois paga. Nec orci ornare consequat. Praesent lacinia ultrices consectetur. Sed non ipsum felis. Quem num gosta di mim que vai caçá sua turmis! In elementis mé pra quem é amistosis quis leo.</p>
+			</section>
+
+		</section>
+		<section className="jobad-details"></section>
+		<section className="company-info"></section>
+		<section className="curriculum-status-mobile">
+			<button>vaga expirada</button>
+		</section>
+		<footer></footer>
+	</div>
     );
   }
 }
